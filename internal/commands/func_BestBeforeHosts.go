4aebfabb9d97410274b63beb9478048baabc27b4
m.eik michalke <meik.michalke@c3s.cc>
2023-11-08 15:02:12 +0100

    preparing for adding a progress bar

--- internal/commands/func_BestBeforeHosts.go
+++ internal/commands/func_BestBeforeHosts.go
@@ -5,15 +5,22 @@ import (
 	"time"
 
 	. "github.com/C3S/certinfo/internal/globals"
+	// "github.com/schollz/progressbar/v3"
 )
 
 func BestBeforeHosts(confTLS *tls.Config) {
 	loc, _ := time.LoadLocation("UTC")
 	now := time.Now().In(loc)
 	keys := sortKeys(AllHosts)
-	hostQueue := make(chan map[string]*CertValidity)
+	hostQueue := make(chan map[string]*CertValidity, 5)
+	// bar := progressbar.Default(int64(len(keys)))
 
 	go func(q chan map[string]*CertValidity) {
+		// for j := 0; j < len(keys); j++ {
+			// thisKey := []string{keys[j]}
+			// q <- daysValid(thisKey, AllHosts, IPversions, Timeout, confTLS, now)
+			// bar.Add(1)
+		// }
 		q <- daysValid(keys, AllHosts, IPversions, Timeout, confTLS, now)
 	}(hostQueue)
 
@@ -23,3 +30,4 @@ func BestBeforeHosts(confTLS *tls.Config) {
 
 	close(hostQueue)
 }
+
