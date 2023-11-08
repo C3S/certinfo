433dcf7dc303bae94ac5f02d3204d8f0cace30c1
m.eik michalke <meik.michalke@c3s.cc>
2023-11-08 21:05:37 +0100

    channels and progressbar seem to work now

--- internal/commands/func_BestBeforeHosts.go
+++ internal/commands/func_BestBeforeHosts.go
@@ -2,32 +2,61 @@ package commands
 
 import (
 	"crypto/tls"
+	"sync"
 	"time"
 
 	. "github.com/C3S/certinfo/internal/globals"
-	// "github.com/schollz/progressbar/v3"
+	"github.com/schollz/progressbar/v3"
 )
 
-func BestBeforeHosts(confTLS *tls.Config) {
+func workerDaysValid(
+	keys []string,
+	now time.Time,
+	jobs <-chan int,
+	results chan<- map[string]*CertValidity,
+	bar *progressbar.ProgressBar,
+	confTLS *tls.Config,
+) {
+	for j := range jobs {
+		thisKey := []string{keys[j-1]}
+		results <- daysValid(thisKey, AllHosts, IPversions, Timeout, confTLS, now)
+		bar.Add(1)
+	}
+}
+
+func BestBeforeHosts(
+	confTLS *tls.Config,
+) {
 	loc, _ := time.LoadLocation("UTC")
 	now := time.Now().In(loc)
 	keys := sortKeys(AllHosts)
-	hostQueue := make(chan map[string]*CertValidity, 5)
-	// bar := progressbar.Default(int64(len(keys)))
+	nKeys := len(keys)
+	jobs := make(chan int, nKeys)
+	results := make(chan map[string]*CertValidity, nKeys)
+	bar := progressbar.NewOptions(
+		len(keys),
+		progressbar.OptionSetDescription("Checking host certificates..."),
+	)
 
-	go func(q chan map[string]*CertValidity) {
-		// for j := 0; j < len(keys); j++ {
-			// thisKey := []string{keys[j]}
-			// q <- daysValid(thisKey, AllHosts, IPversions, Timeout, confTLS, now)
-			// bar.Add(1)
-		// }
-		q <- daysValid(keys, AllHosts, IPversions, Timeout, confTLS, now)
-	}(hostQueue)
-
-	for r := range hostQueue {
-		bestBeforeCheck(r)
+	nWorkers := 4
+	var wg sync.WaitGroup
+	for i := 1; i <= nWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			workerDaysValid(keys, now, jobs, results, bar, confTLS)
+		}()
 	}
 
-	close(hostQueue)
-}
+	for j := 1; j <= nKeys; j++ {
+		jobs <- j
+	}
+	close(jobs)
 
+	wg.Wait()
+	close(results)
+
+	for r := range results {
+		bestBeforeCheck(r)
+	}
+}
